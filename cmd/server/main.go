83f0baac4d2c80ed397bfbe6f7d2f6d2d65e5e0a
wi-cuckoo <yp_liuwei@163.com>
2019-05-02 13:38:25 +0800

    init flags

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
+var flags = []cli.Flag{
+	cli.BoolFlag{
+		EnvVar: "GOAHEAD_DEBUG",
+		Name:   "debug",
+		Usage:  "enable debug mode, default false",
+	},
+	cli.StringFlag{
+		EnvVar: "GOAHEAD_CONF",
+		Name:   "config, c",
+		Usage:  "config file to startup daemon",
+		Value:  "/etc/goahead.conf",
+	},
+}
+
+var commands = []cli.Command{}
+
 func main() {
-	fmt.Println("meta nodes is a raft cluster")
+	app := cli.NewApp()
+	app.Name = "goahead"
+	app.Usage = "control your application, like systemd"
+	app.Version = "unknown"
+	app.Flags = flags
+	app.Action = run
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
 }
---
+++ cmd/server/startup.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/urfave/cli"
+)
+
+func run(c *cli.Context) error {
+	return errors.New("fuck you")
+}
